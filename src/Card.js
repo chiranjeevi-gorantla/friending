cfaea837c2627b311276dd5797a834105fc89a37
chiranjeevi-gorantla <chiranjeevigorantla@gmail.com>
2021-06-02 18:34:22 +0530

    Built_ver_1

--- src/Card.js
+++ src/Card.js
@@ -1,1 +1,16 @@
 import React from 'react';
+
+const Card = (props) => {
+  const {id, name, email} = props;
+  return (
+    <div className='tc bg-dark-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+      <img src={`https://robohash.org/${id}`} alt='profile_photo' width='200px' height='200px'/>
+      <div>
+        <h2>{name}</h2>
+        <p>{email}</p>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
--- src/index.js
+++ src/index.js
@@ -4,10 +4,15 @@ import './index.css';
 import Card from './Card';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
+import {members} from './members';
 
 ReactDOM.render(
-    <Card />,
-  document.getElementById('root')
+               <div>
+                <Card id={members[0].id} name={members[0].name} email={members[0].email} />
+                <Card id={members[1].id} name={members[1].name} email={members[1].email}/>
+                <Card id={members[2].id} name={members[2].name} email={members[2].email}/>
+               </div>
+    , document.getElementById('root')
 );
 
 
